66d18518b961b9b651ee38909c8056162a01f4e8
stone <josias.stone@gmail.com>
2019-01-23 10:25:55 -0200

    espacionei a pergunta

--- project/Program.cs
+++ project/Program.cs
@@ -7,6 +7,8 @@ namespace project
         static void Main(string[] args)
         {
             Console.WriteLine("Olá Mundo");
+            Console.WriteLine("Bom dia");
+            Console.WriteLine("Como Vc Está?");
             Console.ReadLine();
         }
     }
