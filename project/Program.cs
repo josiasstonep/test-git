aade9b6e355cd724cf01cd4631361fc4c07d61b1
stone <josias.stone@gmail.com>
2019-01-22 22:33:53 -0200

    Projeto iniciado, mostrando olá mundo

---
+++ project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo");
+            Console.ReadLine();
+        }
+    }
+}
